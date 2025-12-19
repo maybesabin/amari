7da2239ff2c6cf2d01abefff8a7ca656e5b684b9
maybesabin <highsabin987@gmail.com>
2025-12-19 06:14:35 +0545

    add blog schema

---
+++ app/models/Blog.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const BlogSchema = new mongoose.Schema({
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    featuredImage: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+const Blog = mongoose.model("Blog", BlogSchema)
+
+if (mongoose.models.Blog) {
+    delete mongoose.models.Blog
+}
+
+export default Blog;
